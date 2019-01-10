2fe503aaae1cafe9d0f9ece2a95896dc3ba5d516
root <917152124@qq.com>
2019-01-10 21:12:23 +0800

    add docker image hello-c

---
+++ hello-world/hello.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Description: hello.c
+	> Author: lkong
+	> Mail: lkong@tencent.com 
+	> Created Time: 2019-01-09 15:44:18
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("hello docker!\n");
+}
